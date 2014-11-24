ff15eaa92ca3574ca8b00fcd5e63dc78960720a4
Evita Korņējeva <evita.Kornejeva@tilde.lv>
2014-11-24 09:03:48 +0000

    git-tfs-id: [http://tfs:8080/tfs/DefaultCollection]$/LetsMT!/PresidencyTranslator/desktopApplication;C24606

---
+++ sign_Translator_2015_release.bat
@@ -0,0 +1,18 @@
+set webpage=http://www.tilde.com/
+set version=1.0.0.0
+set logfile=signfile.log
+set frameworkbin=C:\Program Files\Microsoft.NET\SDK\v1.1\Bin
+set timestamp=http://timestamp.verisign.com/scripts/timstamp.dll
+set signature=b5fde1c49d5bda708e155914f0bf7d3e95910be9
+path %path%;%frameworkbin%;c:\blat262\full;
+echo %date% %time%>"%logfile%"
+
+set descr="Translator 2015"
+set file="out\Release_setup\DISK1\Translator2015.exe"
+ chktrust %file% -q
+ if ERRORLEVEL 1	signcode.exe -sha1 %signature% -n %descr% -i %webpage% -t %timestamp% %file% >>"%logfile%"
+ chktrust %file% >>"%logfile%"
+ 
+ 
+copy %logfile% sign.log
+del %logfile%

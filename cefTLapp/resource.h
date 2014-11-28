7deacbf8b3aec1fdcd56d8e2a5e22759ebf0729b
Evita Korņējeva <evita.Kornejeva@tilde.lv>
2014-11-28 09:37:38 +0000

    git-tfs-id: [http://tfs:8080/tfs/DefaultCollection]$/LetsMT!/PresidencyTranslator/desktopApplication;C24701

--- cefTLapp/resource.h
+++ cefTLapp/resource.h
@@ -1,52 +1,39 @@
-// Copyright (c) 2013 The Chromium Embedded Framework Authors. All rights
-// reserved. Use of this source code is governed by a BSD-style license that
-// can be found in the LICENSE file.
-
 //{{NO_DEPENDENCIES}}
 // Microsoft Visual C++ generated include file.
 // Used by cefTLapp.rc
 //
+#define IDI_CEFTLAPP                    100
+#define IDI_SMALL                       101
+#define IDS_APP_TITLE                   110
+#define IDC_CEFCLIENT                   111
 #define BINARY                          256
-#define IDI_CEFTLAPP					100
-#define IDI_SMALL						101
-#define IDS_APP_TITLE					110
-#define IDC_CEFCLIENT					111
-
-#define IDS_MAIN_HTML					1000
-#define IDS_CSS_INDEX					1001
-
-#define IDS_IMG_ARROW					1002
-#define IDS_IMG_DOC_LETSMT				1003
-#define IDS_IMG_DOC_LETSMT_ACTIVE		1004
-#define IDS_IMG_HUGO					1005
-#define IDS_IMG_TEXT_LETSMT				1006
-#define IDS_IMG_TEXT_LETSMT_ACTIVE		1007
-#define IDS_IMG_TILDE_EU2015_LOGOFULL	1008
-#define IDS_IMG_TILDE_EU2015_LOGOSHORT	1009
-#define IDS_IMG_WEB_LETSMT				1010
-#define IDS_IMG_WEB_LETSMT_ACTIVE		1011
-
-#define IDS_JS_ANGULAR_MIN_JS			1101
-#define IDS_JS_ANGULAR_ROUTE_MIN_JS		1102
-#define IDS_JS_ANGULAR_FILTER_MIN_JS	1103
-#define IDS_JQUERY_MIN_JS				1104
-#define IDS_JS_MYPAGECTRL_JS			1105
-#define IDS_JS_MYTRANSLATORAPP_JS		1106
-
-#define IDS_TEMPLATES_ABOUT_HTML		1201
-#define IDS_TEMPLATES_HUGO_HTML			1202
-#define IDS_TEMPLATES_MOBILEAPP_HTML	1203
-#define IDS_TEMPLATES_TRANSL_DOC_HTML	1204
-#define IDS_TEMPLATES_TRANSL_TEXT_HTML	1205
-#define IDS_TEMPLATES_TRANSL_WEB_HTML	1206
-
-#define IDS_TEXT_WIDGET_IMG_CROSS		1301
-#define IDS_TEXT_WIDGET_WIDGET_CSS		1302
-#define IDS_TEXT_WIDGET_CORE_JS			1303
-
-
-// Avoid files associated with MacOS
-#define _X86_
+#define IDS_MAIN_HTML                   1000
+#define IDS_CSS_INDEX                   1001
+#define IDS_IMG_ARROW                   1002
+#define IDS_IMG_DOC_LETSMT              1003
+#define IDS_IMG_DOC_LETSMT_ACTIVE       1004
+#define IDS_IMG_HUGO                    1005
+#define IDS_IMG_TEXT_LETSMT             1006
+#define IDS_IMG_TEXT_LETSMT_ACTIVE      1007
+#define IDS_IMG_TILDE_EU2015_LOGOFULL   1008
+#define IDS_IMG_TILDE_EU2015_LOGOSHORT  1009
+#define IDS_IMG_WEB_LETSMT              1010
+#define IDS_IMG_WEB_LETSMT_ACTIVE       1011
+#define IDS_JS_ANGULAR_MIN_JS           1101
+#define IDS_JS_ANGULAR_ROUTE_MIN_JS     1102
+#define IDS_JS_ANGULAR_FILTER_MIN_JS    1103
+#define IDS_JQUERY_MIN_JS               1104
+#define IDS_JS_MYPAGECTRL_JS            1105
+#define IDS_JS_MYTRANSLATORAPP_JS       1106
+#define IDS_TEMPLATES_ABOUT_HTML        1201
+#define IDS_TEMPLATES_HUGO_HTML         1202
+#define IDS_TEMPLATES_MOBILEAPP_HTML    1203
+#define IDS_TEMPLATES_TRANSL_DOC_HTML   1204
+#define IDS_TEMPLATES_TRANSL_TEXT_HTML  1205
+#define IDS_TEMPLATES_TRANSL_WEB_HTML   1206
+#define IDS_TEXT_WIDGET_IMG_CROSS       1301
+#define IDS_TEXT_WIDGET_WIDGET_CSS      1302
+#define IDS_TEXT_WIDGET_CORE_JS         1303
 
 // Next default values for new objects
 // 

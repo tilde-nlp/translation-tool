2af9e9e9d17f18958ccdaf4fd7dbf3cbb7e00e16
Andis Lagzdiņš <andis.lagzdins@Tilde.lv>
2015-01-09 13:29:20 +0000

    git-tfs-id: [http://tfs:8080/tfs/DefaultCollection]$/LetsMT!/PresidencyTranslator/desktopApplication;C25068

--- sign_Translator_2015_release.bat
+++ sign_Translator_2015_release.bat
@@ -8,7 +8,7 @@ path %path%;%frameworkbin%;c:\blat262\full;
 echo %date% %time%>"%logfile%"
 
 set descr="Translation Tool 2015"
-set file="out\Release_setup\DISK1\NPFoodsTranslator.exe"
+set file="out\Release_setup\DISK1\translate2015.exe"
  chktrust %file% -q
  if ERRORLEVEL 1	signcode.exe -sha1 %signature% -n %descr% -i %webpage% -t %timestamp% %file% >>"%logfile%"
  chktrust %file% >>"%logfile%"
